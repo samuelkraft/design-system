f3028a19174dd816c380e026710bd3f47509b935
Samuel Kraft <samuelkraft@me.com>
2022-04-07 13:49:10 +0200

    Update index.tsx

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import { Stack } from "components/src";
+
 const Home = () => {
-  return <div>welcome to the design-system</div>;
+  return (
+    <section>
+      <Stack direction="column" space="10px">
+        <h1>Design System</h1>
+        <p>Welcome to the design system</p>
+      </Stack>
+    </section>
+  );
 };
 
 export default Home;
